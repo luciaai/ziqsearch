1a6b2895e8bd632661464e7576fad2613fded208
luciaai <luciaai@proton.me>
2026-04-22 23:11:23 -0500

    Fix opengraph image: use proper Ziq logo SVG instead of makeshift version

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -20,26 +20,19 @@ export default async function OgImage() {
           padding: '80px',
         }}
       >
-        {/* Logo - Ziq with lightning bolt dotting the i */}
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'flex-start',
-            fontSize: '180px',
-            fontWeight: 700,
-            color: '#ffffff',
-            fontFamily: 'system-ui, -apple-system, sans-serif',
-            position: 'relative',
-          }}
-        >
-          <span>Z</span>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <svg width="80" height="120" viewBox="0 0 100 150" style={{ marginBottom: '-30px' }}>
-              <path d="M 50 10 L 30 60 L 45 60 L 30 110 L 70 60 L 55 60 Z" fill="#22c55e"/>
-            </svg>
-            <span style={{ marginTop: '0' }}>q</span>
-          </div>
-        </div>
+        {/* Logo - Ziq with lightning bolt */}
+        <svg width="400" height="200" viewBox="0 0 1024 512" xmlns="http://www.w3.org/2000/svg">
+          {/* Z letter - white */}
+          <path d="M 212 223 L 422 223 L 422 283 L 292 413 L 422 413 L 422 503 L 212 503 L 212 443 L 342 313 L 212 313 Z" fill="#ffffff"/>
+          
+          {/* Lightning bolt - green (#22c55e) */}
+          <path d="M 495 95 L 440 220 L 485 220 L 440 345 L 550 220 L 505 220 Z" fill="#22c55e"/>
+          
+          {/* Q letter - white with tail */}
+          <path d="M 680 223 C 620 223 575 268 575 363 C 575 458 620 503 680 503 C 740 503 785 458 785 363 C 785 268 740 223 680 223 Z M 680 283 C 705 283 725 303 725 363 C 725 423 705 443 680 443 C 655 443 635 423 635 363 C 635 303 655 283 680 283 Z" fill="#ffffff"/>
+          {/* Q tail */}
+          <path d="M 710 420 L 785 503" stroke="#ffffff" stroke-width="50" stroke-linecap="round" fill="none"/>
+        </svg>
         
         {/* Tagline */}
         <div
